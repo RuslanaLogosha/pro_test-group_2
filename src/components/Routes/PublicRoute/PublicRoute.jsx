40ed69c643e21ecb5bf7ef52bf039e7bb55da224
Ruslana Logosha <ruslanalogosha@gmail.com>
2021-04-04 00:34:08 +0300

    comments unused vars and fixes lint's rule

--- src/components/Routes/PublicRoute/PublicRoute.jsx
+++ src/components/Routes/PublicRoute/PublicRoute.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 
 // import { authSelectors } from '../redux/auth/';
 
