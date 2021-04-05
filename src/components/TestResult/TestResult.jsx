98ece78f9a7015d8c084c9fdc1e4dcf0a1fd8bff
Aleksey Grygorenko <aleksey2892@gmail.com>
2021-04-06 00:06:54 +0300

    ready base logic for diagram, install npm: "react-minimal-pie-chart"

--- src/components/TestResult/TestResult.jsx
+++ src/components/TestResult/TestResult.jsx
@@ -1,31 +1,41 @@
 import React from 'react';
 import cat from '../../images/result-page-cat.svg';
+import Diagram from './Diagram';
 import s from './TestResult.module.css';
 
-export default function TestResult() {
+export default function TestResult(results) {
+  const {
+    correctAnswers = 30,
+    totalQuestions = 50,
+    aboutResultTitle = 'Not bad!',
+    aboutResultSubtitle = 'But you still need to learn some materials.',
+  } = results;
+
   return (
     <div>
       <div className={s.resultBlock}>
-        <h1 className={s.mainTitle}>Результаты</h1>
-        <h2 className={s.mainSubtitle}>[ Теория Тестирования_]</h2>
-        <div className={s.diagram}>should be diagram here</div>
+        <h1 className={s.mainTitle}>Results</h1>
+        <h2 className={s.mainSubtitle}>[ Testing Theory_]</h2>
+
+        <div className={s.diagram}>
+          <Diagram values={{ correctAnswers, totalQuestions }} />
+        </div>
+
         <div className={s.questionBlock}>
           <p className={s.correctAnswers}>
-            Верных ответов - <span>9</span>
+            Correct answers - <span>{correctAnswers}</span>
           </p>
           <p className={s.totalQuestions}>
-            Всего вопросов - <span>12</span>
+            Total questions - <span>{totalQuestions}</span>
           </p>
         </div>
       </div>
 
       <div className={s.aboutResult}>
         <img src={cat} alt="cat" className={s.image} />
-        <h2 className={s.aboutResultTitle}>Неплохой результат!</h2>
-        <h3 className={s.aboutResultSubtitle}>
-          Но тебе еще нужно доучить материалы.
-        </h3>
-        <button className={s.btnRetry}>Пройти еще раз</button>
+        <h2 className={s.aboutResultTitle}>{aboutResultTitle}</h2>
+        <h3 className={s.aboutResultSubtitle}>{aboutResultSubtitle}</h3>
+        <button className={s.btnRetry}>Try again</button>
       </div>
     </div>
   );
