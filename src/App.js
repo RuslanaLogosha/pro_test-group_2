c68392037f3f649d6a00085eda3b73395698d677
Kateryna-Urbanovych <katya_urb@ukr.net>
2021-04-06 16:01:10 +0300

    add auth redux, install npm axios, fix App.js, RegisterView and Form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Footer from './components/Footer/Footer';
@@ -8,19 +9,30 @@ import SpinnerLoader from './components/SpinnerLoader';
 import RegisterView from './views/RegisterView';
 import MaterialsView from './components/MaterialsView/MaterialsView';
 import ContactsView from './components/Contacts/ContactsView';
-
+import { authOperations, authSelectors } from 'redux/auth';
 
 // import PrivateRoute from './routes/PrivateRoute';
 // import PublicRoute from './routes/PublicRoute';
 
 function App() {
-  return (
+  const dispatch = useDispatch();
+  const isFetchingCurrentUser = useSelector(
+    authSelectors.getIsFetchingCurrentUser,
+  );
+
+  useEffect(() => {
+    dispatch(authOperations.fetchCurrentUser());
+  }, [dispatch]);
+
+  return isFetchingCurrentUser ? (
+    <h1>Show React Skeleton</h1>
+  ) : (
     <>
       <BrowserRouter>
         <Header isLoggedIn={true} userName="Vladymyr" />
         <Switch>
           <Suspense fallback={<SpinnerLoader />}>
-            <Route path="/register" component={RegisterView} />
+            <Route path="/register" restricted component={RegisterView} />
             <Route
               exact
               path="/"
